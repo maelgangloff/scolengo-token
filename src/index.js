0d8a11e5af5fc7ddaf7f855f7257885625287673
Maël Gangloff <contact@maelgangloff.fr>
2023-04-28 16:38:19 +0200

    feat: hide MenuBar. Ref #3

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
