8c07aa467725daf2c5bd7d98ee9f1bb1efef4d57
nonozgytb <nonozg.ytb@gmail.com>
2023-04-28 03:59:26 +0200

    feat: add list school + dark theme + download config + other fixs
    Add the list of differents school on searching, Add the dark theme support, Add download config file link, Add build command, Add NonozgYtb to contributors

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  setSchool: (schoolName) => ipcRenderer.send('school-name', schoolName),
-  sendToken: (token) => ipcRenderer.on('send-token', token)
+  setSearch: (schoolName) => ipcRenderer.send('school-search', schoolName),
+  onSchoolList: (callback) => ipcRenderer.on('school-list', callback),
+  sendToken: (token) => ipcRenderer.on('send-token', token),
+  schoolAuth: (school) => ipcRenderer.send('school-auth', school)
 })
