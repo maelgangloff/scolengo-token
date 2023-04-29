7e9d85966da918c1f35f854fa0c04b1998f9c693
Maël Gangloff <contact@maelgangloff.fr>
2023-04-29 16:00:48 +0200

    fix: remove CEDEX

--- src/renderer.js
+++ src/renderer.js
@@ -10,24 +10,15 @@ form.addEventListener('submit', (e) => {
 window.addEventListener('load', () => {
   window.electronAPI.onSchoolList((_, schools) => {
     schoolsElem.innerHTML = ''
-    schools.forEach((school) => {
-      const schooHTML = `<b class="school-name">${
-                      school.name
-                  }</b><span class="school-ems">(${
-                      school.emsCode
-                  })</span>-<i class="school-city">${school.city
-                      .replace(/CEDEX ([0-9]+)/, '')
-                      .trim()} ${
-                      school.zipCode
-                  }</i><button class="school-select">Choisir</button>`
+    schools.forEach(school => {
+      const schooHTML = `<b class="school-name" title="${school.id}">${school.name}</b>
+      <span class="school-ems">(${school.emsCode})</span> -
+      <i class="school-city">${school.city.replace('CEDEX', '').trim()} ${school.zipCode}</i>
+      <button class="school-select">Choisir</button>`
+
       const li = document.createElement('li')
       li.innerHTML = schooHTML
-      li.querySelector('.school-select').addEventListener(
-        'click',
-        async () => {
-          window.electronAPI.schoolAuth(school)
-        }
-      )
+      li.querySelector('button').addEventListener('click', () => window.electronAPI.schoolAuth(school))
       schoolsElem.appendChild(li)
     })
   })
